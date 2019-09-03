bf9bf3ca08238ab971d1688c2c976841e5dafd4c
Your NameSymPiTo <pito@online.de>
2019-09-03 11:04:37 +0200

    f

--- MyIpsWebHook/www/js/myVars.js
+++ MyIpsWebHook/www/js/myVars.js
@@ -997,25 +997,25 @@
       DisModeRolloB.update(sym[21]);
       DisST1RolloB.update(sym[55]);
       DisST2RolloB.update(sym[56]);
-      CbSSrolloB.update(sym[57]);
+      //CbSSrolloB.update(sym[57]);
 
       DisPosRolloK.update(sym[15]);
       DisModeRolloK.update(sym[14]);
       DisST1RolloK.update(sym[50]);
       DisST2RolloK.update(sym[51]);
-      CbSSrolloK.update(sym[52]);
+      //CbSSrolloK.update(sym[52]);
 
       DisPosRolloSZ.update(sym[114]);
       DisModeRolloSZ.update(sym[113]);
       DisST1RolloSZ.update(sym[116]);
       DisST2RolloSZ.update(sym[117]);
-      CbSSrolloSZ.update(sym[118]);
+      //CbSSrolloSZ.update(sym[118]);
 
       DisPosRolloKZ.update(sym[13]);
       DisModeRolloKZ.update(sym[12]);
       DisST1RolloKZ.update(sym[122]);
       DisST2RolloKZ.update(sym[119]);
-      CbSSrolloKZ.update(sym[120]);
+      //CbSSrolloKZ.update(sym[120]);
 
 
 
