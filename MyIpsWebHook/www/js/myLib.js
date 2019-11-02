3b918e94fb3dee7939768ddf7a2f6a055223235e
Your NameSymPiTo <pito@online.de>
2019-11-02 10:13:44 +0100

    aa

--- MyIpsWebHook/www/js/myLib.js
+++ MyIpsWebHook/www/js/myLib.js
@@ -3823,7 +3823,7 @@ class DynIconList {
                 } else if (source === "CD") {
                     var cmd = "command(DenonCeol,loadCDPlaylist," + item['FV'] + ")";
                 } else if (source === "Audio") {
-                    var cmd = "command(DenonCeol,loadAudioPlaylist," + item['FV'] + ")";
+                    var cmd = "command(DenonCeol,loadAudioPlaylist," + item['playlistname'] + ")";
                 }
                 send(cmd);
             };
