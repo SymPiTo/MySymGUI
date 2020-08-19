fdb76ce9ab67203bf470e33c33fbb0635d3868cc
GitIPS <torsten.beck@@onlinehome.de>
2020-08-19 17:37:51 +0200

    ss

--- MyIpsWebHook/www/js/myCommands.js
+++ MyIpsWebHook/www/js/myCommands.js
@@ -1,15 +1,3 @@
-function toggle() {
-    var button = document.querySelector('.toggle');
-    var overlay = document.querySelector('.glass');
-    if (overlay.className === 'glass down') {
-        overlay.className = 'glass up';
-        button.innerText = '-';
-    } else {
-        overlay.className = 'glass down';
-        button.innerText = '+';
-    }
-}
-
 function ShowHidePanel(no) {
     switch (no) {
         case '1on':
