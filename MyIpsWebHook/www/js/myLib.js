b1aacc87aaec717c3675230baeb9c4686de224a7
GitIPS <torsten.beck@@onlinehome.de>
2020-08-20 19:22:39 +0200

    ww

--- MyIpsWebHook/www/js/myLib.js
+++ MyIpsWebHook/www/js/myLib.js
@@ -4406,7 +4406,7 @@ class Message {
 
         var elemText = document.createElement("div");
         this.IDText = elemText;
-        elemText.style.lineHeight = "10px";
+        elemText.style.lineHeight = "24px";
         elemText.innerHTML = ""
 
         container.appendChild(elemText);
