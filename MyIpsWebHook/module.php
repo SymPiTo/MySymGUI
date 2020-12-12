56c449cd4e04d495f76142a856178cca78264a34
GitIPS <torsten.beck@@onlinehome.de>
2020-12-12 19:09:26 +0100

    d

--- MyIpsWebHook/module.php
+++ MyIpsWebHook/module.php
@@ -79,7 +79,7 @@ if(!isset($_SERVER['PHP_AUTH_PW']))
                         include_once($path);
                 } else {
                    header("Content-Type: ".$this->GetMimeType($extension));
-                   header("Link: </css/myGlides.css>; rel=preload; as=style, </js/myGlides.js>; rel=preload; as=script");
+                   header("Link: </hook/myipshook/css/myGlides.css>; rel=preload; as=style, </hook/myipshook/js/myGlides.js>; rel=preload; as=script");
                     readfile($path);
                 }
                 
