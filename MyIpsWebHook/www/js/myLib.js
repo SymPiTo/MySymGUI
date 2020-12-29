08ac93ab7468a1e7198bc6d2a27ca59ed9c34d68
GitIPS <torsten.beck@@onlinehome.de>
2020-12-29 17:53:12 +0100

    rr

--- MyIpsWebHook/www/js/myLib.js
+++ MyIpsWebHook/www/js/myLib.js
@@ -4439,8 +4439,8 @@ class MyChart {
 
 
     create(ParentID, scriptID, unit1, unit2 = '', ds0, ds1 = '', ds2 = '', ds3 = '', ...param) {
-        var b = "80%";
-        var h = "160%";
+        var b = "700px";
+        var h = "350";
         var top = "250px";
         var left = "10px";
 
